f2acd000c473e302151cd1cb09d8f5b78650ee66
Gurguii <airangomez@hotmail.com>
2024-06-12 14:45:17 +0100

    useful state

--- url_enc_dec.hpp
+++ url_enc_dec.hpp
@@ -1,47 +1,62 @@
 #pragma once
+
 #include <cstdint>
 #include <string>
 #include <cstring>
 #include <sstream>
 
-static constexpr const char *ASCII_RESERVED = "!#$&'()*+,/:;=?@[]";
-static constexpr const int ASCII_RESERVED_COUNT = 18;
+inline static constexpr const char *ASCII_RESERVED = "!#$&'()*+,/:;=?@[]";
+inline static constexpr const int ASCII_RESERVED_COUNT = 18;
 
-static constexpr const char *ASCII_SPECIAL = "\"%-.<>\\^_`{|}~";
-static constexpr const int ASCII_SPECIAL_COUNT = 14;
+inline static constexpr const char *ASCII_SPECIAL = "\"%-.<>\\^_`{|}~";
+inline static constexpr const int ASCII_SPECIAL_COUNT = 14;
 
-static inline const auto int_to_hex = [](const uint8_t &byte, std::ostringstream &buff){
+inline static const auto int_to_hex = [](const uint8_t &byte, std::ostringstream &buff){
   static constexpr const char *hex_vals = "0123456789abcdef";
   buff << '%';
   buff << hex_vals[byte/16];
   buff << hex_vals[byte%16];
+  return true;
 };
 
-static inline const auto hex_to_int = [](const char *byte, std::ostringstream &buff){
+inline static const auto hex_to_int = [](const char *byte, std::ostringstream &buff){
   const char &first_half = *byte;
   const char &second_half = *(byte+1);
   uint8_t digit;
 
   if(first_half >= '0' && first_half <= '9'){
-    digit = first_half * 16;
+    digit = (first_half - '0') * 16;
   }else if(first_half >= 'a' && first_half <= 'f'){
-    digit = 'f' - first_half + 10 * 16; 
+    digit = (first_half - 'a' + 10) * 16;
   }else if(first_half >= 'A' && first_half <= 'F'){
-    digit = 'F' - first_half + 10 * 16;
+    digit = (first_half - 'A' + 10) * 16;
   }
 
   if(second_half >= '0' && second_half <= '9'){
-    digit += second_half;
+    digit += second_half - '0';
   }else if(second_half >= 'a' && second_half <= 'f'){
-    digit += 'f' - second_half + 15;
+    digit += second_half - 'a' + 10;
   }else if(second_half >= 'A' && second_half <= 'F'){
-    digit += 'F' - second_half + 15;
+    digit += second_half - 'A' + 10;
   }
 
-  buff << digit;
+  buff << static_cast<char>(digit);
 };
 
 namespace url::encode{
+  enum class charset : uint8_t{
+    special  =   1 << 0 & 0xff,
+    reserved =   1 << 1 & 0xff,
+    all      =  (1 << 2 & 0xff) - 1,
+    none     =   0
+  };
+  static inline bool operator&(charset lo, charset ro){
+    return static_cast<bool>(static_cast<uint8_t>(lo) & static_cast<uint8_t>(ro));
+  }
+  static inline void operator|=(charset &lo, charset ro){
+    lo = static_cast<charset>(static_cast<uint8_t>(lo) | static_cast<uint8_t>(ro));
+  }
+
   static inline std::string full(const char *data, size_t const data_size){
     std::ostringstream result{};
     
@@ -52,31 +67,22 @@ namespace url::encode{
     return result.str();
   }
   
-  static inline std::string special(const char *data, size_t data_size){
+
+  static inline std::string partial(const char *data, size_t data_size, charset desired){
     std::ostringstream result{};
 
     for(size_t i = 0; i < data_size; ++i){
-      if( memchr(ASCII_SPECIAL, data[i], ASCII_SPECIAL_COUNT) == nullptr){
-        result <<  data[i];
-      }else{
-        int_to_hex(data[i],result);
+      if(desired & charset::special && memchr(ASCII_SPECIAL, data[i], ASCII_SPECIAL_COUNT) != nullptr){
+        int_to_hex(data[i], result);
+        continue;
       }
+      if(desired & charset::reserved && memchr(ASCII_RESERVED, data[i], ASCII_RESERVED_COUNT) != nullptr){
+        int_to_hex(data[i], result);
+        continue;
+      }
+      result << data[i];
     }
 
-    return result.str();
-  }
-  
-  static inline std::string reserved(const char *data, size_t data_size){
-    std::ostringstream result{};
-  
-    for(size_t i = 0; i < data_size; ++i){
-      if( memchr(ASCII_RESERVED, data[i], ASCII_RESERVED_COUNT) == nullptr){
-        result << data[i];
-      }else{
-        int_to_hex(data[i],result);
-      }
-    }
-  
     return result.str();
   }
 }
--- url_encode.cpp
+++ url_encode.cpp
@@ -11,6 +11,7 @@
 static bool ENCODE = true;
 static bool FULL = false;
 static bool SPECIAL = false;
+static url::encode::charset DESIRED_CHARSET = url::encode::charset::none;
 
 static inline int __available_on_stdin(){
   int available = 0;
@@ -32,20 +33,21 @@ static inline void usage(){
 url [OPTIONS]
 
 ** Options **
-
 -d, --decode      : Encodes all characters that may cause issues in URLs.
 -f, --full        : Encodes characters reserved in the URL syntax (e.g., '&', '+').
--s, --special     : Encodes characters that might be misinterpreted in URLs (e.g., spaces, '$').
+-s, --special     : Encodes ascii special characters  - \"%-.<>\\^_`{|}~
+-r, --reserved    : Encodes ascii reserved characters - !#$&'()*+,/:;=?@[]
 
 ** Examples **
-url <<< https://github.com/Gurguii/gpkih
-
+url -f    <<< '/my_/$foo_example' - %2f%6d%79%5f%2f%24%66%6f%6f%5f%65%78%61%6d%70%6c%65%0a
+url -r    <<< '/my_/$foo_example' - %2fmy_%2f%24foo_example
+url -s    <<< '/my_/$foo_example' - /my%5f/$foo%5fexample
+url -s -r <<< '/my_/$foo_example' - %2fmy%5f%2f%24foo%5fexample
 )");
 }
 
 
 int main(int argc, const char **args) {
-
   for(int i = 1; i < argc; ++i){
     std::string_view opt = args[i];
     if("-d" == opt || "--decode" == opt){
@@ -53,18 +55,19 @@ int main(int argc, const char **args) {
     }else if("-f" == opt || "--full" == opt){
       FULL = true;
     }else if("-s" == opt || "--special" == opt){
-      SPECIAL = true;
-    }else if("-h" == opt | "--help" == opt){
+      DESIRED_CHARSET |= url::encode::charset::special;
+    }else if("-r" == opt || "--reserved" == opt){
+      DESIRED_CHARSET |= url::encode::charset::reserved;
+    }else if("-h" == opt || "--help" == opt){
       usage(); return 0;
     }else{
       fprintf(stderr, "-- Unknown option '%s'\n", args[i]);
     }
   }
-
   int available = __available_on_stdin();
   
   if(available <= 0){
-    fprintf(stderr, "[!] Got nothing in stdin to %s\n", ENCODE ? "encode" : "decode");
+    fprintf(stderr, "[!] Got nothing in stdin to %s, use -h | --help to display help\n", ENCODE ? "encode" : "decode");
     return 1;
   }
 
@@ -81,9 +84,14 @@ int main(int argc, const char **args) {
   };
 
   std::string result;
-  
+
   if(ENCODE){
-    result = FULL ? url::encode::full(reinterpret_cast<const char*>(data), available) : url::encode::reserved(reinterpret_cast<const char*>(data), available);
+    if(FULL == false && DESIRED_CHARSET == url::encode::charset::none){
+      fprintf(stderr, "[!] No encoding option given, use -h | --help to see available options\n");
+      return 1;
+    }
+    result = FULL ? url::encode::full(reinterpret_cast<const char*>(data), available)
+                  : url::encode::partial(reinterpret_cast<const char*>(data), available, DESIRED_CHARSET);
   }else{
     result = url::decode::full(reinterpret_cast<const char*>(data), available);
   }
