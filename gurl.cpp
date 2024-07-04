d30fa2640c2baa852f5cc954bba18cf84c9ffa5f
Gurguii <airangomez@hotmail.com>
2024-07-04 21:08:24 +0000

    Added -c | --charset for user defined charset to encode, improved header file by overloading encoding/decoding functions with/out FILE*

--- gurl.cpp
+++ gurl.cpp
@@ -1,7 +1,6 @@
 #include "url_enc_dec.hpp"
 #include <cstdio>
-#include <errno.h>
-#include <string.h>
+#include <cstring>
 
 #ifdef _WIN32
 #include <Windows.h>
@@ -17,7 +16,14 @@ static bool SPECIAL  = false;
 static FILE *INFILE  = stdin;
 static FILE *OUTFILE = stdout;
 
-static url::encode::charset ENCODE_CHARSET = url::encode::charset::reserved;
+inline static constexpr const char *ASCII_RESERVED = "!#$&'()*+,/:;=?@[]";
+inline static constexpr const int ASCII_RESERVED_COUNT = 18;
+
+inline static constexpr const char *ASCII_SPECIAL = "\"%-.<>\\^_`{|}~";
+inline static constexpr const int ASCII_SPECIAL_COUNT = 14;
+
+static char ENCODE_CHARSET[255]{};
+static int ENCODE_CHARSET_SIZE = 0;
 
 static inline int __available_on_stdin(){
   unsigned long available = 0;
@@ -48,26 +54,26 @@ URL encode or decode data
 Note: by default the program will encode reserved characters, the -r option
 only makes sense if -s | --special is given and you also want to encode reserved characters.
   
--d, --decode      : Decode
--r, --reserved    : Encodes ASCII reserved characters %s
--s, --special     : Encodes ASCII special characters %s
--f, --full        : Encodes every character
--o, --out <file>  : Output data to file instead of stdout
--i, --in  <file>  : Read data from file instead of stdin
- 
+-d, --decode         : Decode
+-r, --reserved       : Encodes ASCII reserved characters %s
+-s, --special        : Encodes ASCII special characters %s - https://datatracker.ietf.org/doc/rfc3986/
+-f, --full           : Encodes every character
+-o, --out     <file> : Output data to file instead of stdout
+-i, --in      <file> : Read data from file instead of stdin
+-c, --charset <str>  : Custom charset to encode
+
 ** Examples **
-echo '/my_/$foo_example' | gurl       => %%2fmy_%%2f%%24foo_example
-echo '/my_/$foo_example' | gurl -s    => /my%%5f/$foo%%5fexample
-echo '/my_/$foo_example' | gurl -f    => %%2f%%6d%%79%%5f%%2f%%24%%66%%6f%%6f%%5f%%65%%78%%61%%6d%%70%%6c%%65%%0a
-echo '/my_/$foo_example' | gurl -r -s => %%2fmy%%5f%%2f%%24foo%%5fexample
+echo '/my_/$foo_example' | gurl        => %%2fmy_%%2f%%24foo_example
+echo '/my_/$foo_example' | gurl -s     => /my%%5f/$foo%%5fexample
+echo '/my_/$foo_example' | gurl -f     => %%2f%%6d%%79%%5f%%2f%%24%%66%%6f%%6f%%5f%%65%%78%%61%%6d%%70%%6c%%65%%0a
+echo '/my_/$foo_example' | gurl -c fo$ => /my_/%%24%%66%%6f%%6f_example
+echo '/my_/$foo_example' | gurl -r -s  => %%2fmy%%5f%%2f%%24foo%%5fexample
 gurl -f -i /etc/passwd -o encoded
 gurl -d -i encoded -o decoded
 )",ASCII_RESERVED, ASCII_SPECIAL);
 }
 
 static int parse(int argc, const char **args){
-  bool user_wants_reserved = false;
-
   for(int i = 1; i < argc; ++i){
     std::string_view opt = args[i];
     if("-d" == opt || "--decode" == opt){
@@ -75,39 +81,57 @@ static int parse(int argc, const char **args){
     }else if("-f" == opt || "--full" == opt){
       FULL = true;
     }else if("-r" == opt || "--reserved" == opt){
-      user_wants_reserved = true;
-      ENCODE_CHARSET += url::encode::charset::reserved;
-    }else if("-s" == opt || "--special" == opt){
-      if(user_wants_reserved){
-        ENCODE_CHARSET += url::encode::charset::special;  
-      }else{
-        ENCODE_CHARSET = url::encode::charset::special;
+      if(ENCODE_CHARSET_SIZE + ASCII_RESERVED_COUNT >= 255){
+        fprintf(stderr, "-- Charset exceeds maximum size (255)\n");
+        return -1;
       }
+      memcpy(ENCODE_CHARSET+ENCODE_CHARSET_SIZE, ASCII_RESERVED, ASCII_RESERVED_COUNT);
+      ENCODE_CHARSET_SIZE+=ASCII_RESERVED_COUNT;
+    }else if("-s" == opt || "--special" == opt){
+      if(ENCODE_CHARSET_SIZE + ASCII_SPECIAL_COUNT >= 255){
+        fprintf(stderr, "-- Charset exceeds maximum size (255)\n");
+        return -1;
+      }
+      memcpy(ENCODE_CHARSET+ENCODE_CHARSET_SIZE, ASCII_SPECIAL, ASCII_SPECIAL_COUNT);
+      ENCODE_CHARSET_SIZE+=ASCII_SPECIAL_COUNT;
     }else if("-h" == opt || "--help" == opt){
       usage(); return 1;
     }else if("-o" == opt || "--out" == opt){
       if(i+1 == argc){
-        fprintf(stderr, "-- Missing argument for '%s'",opt.data()); return 1;
+        fprintf(stderr, "-- Missing argument for '%s'\n",opt.data()); return 1;
       }
       OUTFILE = fopen(args[++i], "wb");
       if(OUTFILE == nullptr){
-        perror("-- Couldn't open OUTPUT file");
-        return 1;
+        perror("-- Couldn't open OUTPUT file\n"); return 1;
       }
     }else if("-i" == opt || "--infile" == opt){
       if(i+1 == argc){
-        fprintf(stderr, "-- Missing argument for '%s'",opt.data()); return 1;
+        fprintf(stderr, "-- Missing argument for '%s'\n",opt.data()); return 1;
       }
       INFILE = fopen(args[++i], "rb");
       if(INFILE == nullptr){
-        perror("-- Couldn't open INPUT file");
-        return 1;
+        perror("-- Couldn't open INPUT file\n"); return 1;
       }
+    }else if("-c" == opt || "--charset" == opt){
+      if(i+1 == argc){
+        fprintf(stderr, "-- Missing argument for '%s'\n", opt.data()); return 1;
+      }
+      size_t charsetSize = strlen(args[++i]);
+      if((ENCODE_CHARSET_SIZE + charsetSize) >= 255){
+        fprintf(stderr, "-- Charset exceeds maximum size (255)\n");
+        return -1;
+      }
+      memcpy(ENCODE_CHARSET+ENCODE_CHARSET_SIZE,args[i],charsetSize);
+      ENCODE_CHARSET_SIZE+=charsetSize;
     }else{
       fprintf(stderr, "-- Unknown option '%s'\n", args[i]);
     }
   }
 
+  if(ENCODE_CHARSET_SIZE == 0){
+    memcpy(ENCODE_CHARSET, ASCII_RESERVED, ASCII_RESERVED_COUNT);
+  }
+  
   return 0;
 }
 
@@ -121,7 +145,7 @@ int main(int argc, const char **args) {
   available = INFILE == stdin ? __available_on_stdin() : __available_on_file(INFILE);
   
   if(available <= 0){
-    fprintf(stderr, "[!] Got nothing in stdin to %s, use -h | --help to display help\n", ENCODE ? "encode" : "decode");
+    fprintf(stderr, "[!] Got no data to %s, use -h | --help to display help\n", ENCODE ? "encode" : "decode");
     return 1;
   }
 
@@ -137,19 +161,18 @@ int main(int argc, const char **args) {
       perror("Reading data to buffer");
       return 1;
     }
+    // This is probably an unexpected END OF FILE
+    // which might happen if using stdin in Windows (Fuck Windows)
+    // so no worries
   };
 
   std::string result;
 
   if(ENCODE){
-    if(FULL == false && ENCODE_CHARSET == url::encode::charset::none){
-      fprintf(stderr, "[!] No encoding option given, use -h | --help to see available options\n");
-      return 1;
-    }
-    result = FULL ? url::encode::full(reinterpret_cast<const unsigned char*>(data), available, OUTFILE)
-                  : url::encode::partial(reinterpret_cast<const unsigned char*>(data), available, ENCODE_CHARSET, OUTFILE);
+    FULL ? url::encode::full(data, available, OUTFILE)
+         : url::encode::partial(data, available, ENCODE_CHARSET,ENCODE_CHARSET_SIZE, OUTFILE);
   }else{
-    result = url::decode::full(reinterpret_cast<const unsigned char*>(data), available, OUTFILE);
+    url::decode::full(reinterpret_cast<const char*>(data), available, OUTFILE);
   }
 
   if(INFILE != stdin){
--- url_enc_dec.hpp
+++ url_enc_dec.hpp
@@ -1,95 +1,101 @@
 #pragma once
+
+#include <cstdint>
 #include <cstring>
 #include <sstream>
 
-static constexpr const char *ASCII_RESERVED = "!#$&'()*+,/:;=?@[]";
-static constexpr const int   ASCII_RESERVED_COUNT = 18;
-
-static constexpr const char *ASCII_SPECIAL = "\"%-.<>\\^_`{|}~";
-static constexpr const int   ASCII_SPECIAL_COUNT = 14;
 static constexpr const char *hex_vals = "0123456789ABCDEF";
 
-static inline void htoi(const unsigned char *byte, std::ostringstream &buff){
-  const unsigned char &first_half = byte[0];
-  const unsigned char &second_half = byte[1];
-  unsigned char digit = 0;
-  
-  first_half >= '0' && first_half <= '9' && (digit = (first_half - '0') * 16)
-  || first_half >= 'a' && first_half <= 'f' && (digit = (first_half - 'a' + 10) * 16)
-  || first_half >= 'A' && first_half <= 'F' && (digit = (first_half - 'A' + 10) * 16);
+inline static const auto hex2byte = [](const char *byte, std::basic_ostringstream<char> &buff){
+  const char &first_half = *byte;
+  const char &second_half = *(byte+1);
+  uint8_t digit;
 
-  second_half >= '0' && second_half <= '9' && (digit += second_half - '0')
-  || second_half >= 'a' && second_half <= 'f' && (digit += second_half - 'a' + 10)
-  || second_half >= 'A' && second_half <= 'F' && (digit += second_half - 'A' + 10);
+  if(first_half >= '0' && first_half <= '9'){
+    digit = (first_half - '0') * 16;
+  }else if(first_half >= 'a' && first_half <= 'f'){
+    digit = (first_half - 'a' + 10) * 16;
+  }else if(first_half >= 'A' && first_half <= 'F'){
+    digit = (first_half - 'A' + 10) * 16;
+  }
+
+  if(second_half >= '0' && second_half <= '9'){
+    digit += second_half - '0';
+  }else if(second_half >= 'a' && second_half <= 'f'){
+    digit += second_half - 'a' + 10;
+  }else if(second_half >= 'A' && second_half <= 'F'){
+    digit += second_half - 'A' + 10;
+  }
 
   buff << digit;
 };
 
 namespace url::encode{
-  enum class charset : unsigned char{
-    special  =   1 << 0 & 0xff,
-    reserved =   1 << 1 & 0xff,
-    all      =  (1 << 2 & 0xff) - 1,
-    none     =   0
-  };
-  static inline bool operator&(charset lo, charset ro){
-    return static_cast<bool>(static_cast<unsigned char>(lo) & static_cast<unsigned char>(ro));
-  }
-  static inline void operator+=(charset &lo, charset ro){
-    lo = static_cast<charset>(static_cast<unsigned char>(lo) | static_cast<unsigned char>(ro));
-  }
-  static inline void operator-=(charset &lo, charset ro) {
-    lo = static_cast<charset>(static_cast<unsigned char>(lo) & ~static_cast<unsigned char>(ro));
-  }
-
-  static inline std::string full(const unsigned char *data, size_t const data_size, FILE *outFile = nullptr){
-    std::ostringstream result{};
+  static inline void full(unsigned char *data, size_t const dataSize, FILE* outPath){
+    std::basic_ostringstream<char> result{};
     
-    for(size_t i = 0; i < data_size; ++i){
+    for(size_t i = 0; i < dataSize; ++i){
       result << '%' << hex_vals[data[i]/16] << hex_vals[data[i]%16];
     }
-    
-    if(outFile != nullptr){
-      fwrite(&result.str()[0], result.str().size(), 1, outFile);  
-    }
-    
-    return result.str();
-  }
   
-  static inline std::string partial(const unsigned char *data, size_t data_size, charset desired, FILE *outFile = nullptr){
-    std::ostringstream result{};
-    for(size_t i = 0; i < data_size; ++i){
-      if(desired & charset::special  && memchr(ASCII_SPECIAL, data[i], ASCII_SPECIAL_COUNT)!= nullptr && result << '%' << hex_vals[data[i]/16] << hex_vals[data[i]%16]){
-        continue;
-      }
-      if(desired & charset::reserved  && memchr(ASCII_RESERVED, data[i], ASCII_RESERVED_COUNT)!= nullptr && result << '%' << hex_vals[data[i]/16] << hex_vals[data[i]%16]){
-        continue;
-      }
-      result << data[i];
-    }
+    fwrite(&result.str()[0], result.str().size(), 1, outPath);
+  }
+
+  static inline std::basic_ostringstream<char> full(unsigned char *data, size_t const dataSize){
+    std::basic_ostringstream<char> result{};
     
-    if(outFile != nullptr){
-      fwrite(&result.str()[0], result.str().size(), 1, outFile);  
+    for(size_t i = 0; i < dataSize; ++i){
+      result << '%' << hex_vals[data[i]/16] << hex_vals[data[i]%16];
+    }
+  
+    return result;
+  }
+
+  static inline void partial(unsigned char *data, size_t dataSize, const char *charset, size_t charsetSize, FILE *outPath){
+    std::basic_ostringstream<char> result{};
+
+    for(size_t i = 0; i < dataSize; ++i){
+      memchr(charset, data[i], charsetSize) != nullptr && result << '%' << hex_vals[data[i]/16] << hex_vals[data[i]%16] || result << data[i];
     }
 
-    return result.str();
+    fwrite(&result.str()[0], result.str().size(), 1, outPath);
+  }
+
+  static inline std::basic_ostringstream<char> partial(unsigned char *data, size_t dataSize, const char *charset, size_t charsetSize){
+    std::basic_ostringstream<char> result{};
+
+    for(size_t i = 0; i < dataSize; ++i){
+      memchr(charset, data[i], charsetSize) != nullptr && result << '%' << hex_vals[data[i]/16] << hex_vals[data[i]%16] || result << data[i];
+    }
+
+    return result;
   }
 }
 
 namespace url::decode{
-  static inline std::string full(const unsigned char *data, size_t const data_size, FILE *outFile = nullptr){
-    std::ostringstream result{};
-    for(int i = 0; i < data_size; ++i){
+  static inline void full(const char *data, size_t const dataSize, FILE* outFile){
+    std::basic_ostringstream<char> result{};
+    for(int i = 0; i < dataSize; ++i){
       if(data[i] == '%'){
-        htoi(data+i+1,result);
+        hex2byte(data+i+1,result);
         i+=2;
       }else{
         result << data[i];
       }
     }
-    if(outFile != nullptr){
-      fwrite(&result.str()[0], result.str().size(), 1, outFile);  
+    fwrite(&result.str()[0], result.str().size(), 1, outFile);
+  }
+
+  static inline std::basic_ostringstream<char> full(const char *data, size_t const dataSize){
+    std::basic_ostringstream<char> result{};
+    for(int i = 0; i < dataSize; ++i){
+      if(data[i] == '%'){
+        hex2byte(data+i+1,result);
+        i+=2;
+      }else{
+        result << data[i];
+      }
     }
-    return result.str();
+    return result;
   }
 }
